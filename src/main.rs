c152bf2438d0201f2dfd66dfc94770bc9468ac5c
fuzzyqu <fuzzyqu@protonmail.com>
2018-03-12 03:17:29 -0400

    rom agora é carrega na memória :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+
 struct Chip8 {
     memory: [u8; 4096],
     v: [u8; 16], // registers
@@ -14,9 +17,9 @@ impl Chip8 {
     fn new() -> Self {
         Chip8 {
             memory: [0; 4096],
-            v: [0; 16], // registers
-            i: 0,       // address register
-            pc: 0x200,  // chip 8 programs start at position 512
+            v: [0; 16],
+            i: 0,
+            pc: 0x200, // chip 8 programs start at position 512
             stack: [0; 16],
             delay_timer: 0,
             sound_timer: 0,
@@ -25,11 +28,36 @@ impl Chip8 {
         }
     }
 
+    fn load_rom(&mut self, rom: &[u8]) {
+        for i in 0..rom.len() {
+            self.memory[512 + i] = rom[i];
+        }
+    }
+
     fn cycle(&mut self) {
-        
+        let opcode = (u16::from(self.memory[self.pc as usize]) << 8)
+            | u16::from(self.memory[self.pc as usize + 1]);
+
+        println!("opcode: {:X}", opcode);
+
+        match opcode & 0xF000 {
+            _ => panic!("unknown opcode {:x}", opcode),
+        }
+        self.pc += 2; // go to the next instruction
     }
 }
 
 fn main() {
+    let mut f = File::open("test.ch8").unwrap();
+    let mut buf = Vec::new();
+
+    f.read_to_end(&mut buf).unwrap();
+
     let mut chip8 = Chip8::new();
+
+    chip8.load_rom(&buf);
+
+    loop {
+        chip8.cycle();
+    }
 }
