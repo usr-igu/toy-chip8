655f7ff078da65494bb5d6dc13036a5d2556088c
sydy <sydy@localhost.localdomain>
2018-03-13 02:30:19 -0400

    simplificações no código

--- src/chip8.rs
+++ src/chip8.rs
@@ -70,6 +70,12 @@ impl Cpu {
 
         self.draw_flag = false;
 
+        let x = (opcode & 0x0F00) >> 8;
+        let y = (opcode & 0x00F0) >> 4;
+        let n = opcode & 0x000F;
+        let nn = opcode & 0x00FF;
+        let nnn = opcode & 0x0FFF;
+
         match opcode & 0xF000 {
             0x0000 => {
                 match opcode & 0x00FF {
@@ -92,83 +98,61 @@ impl Cpu {
             }
             //1NNN Jump to address NNN
             0x1000 => {
-                let nnn = opcode & 0x0FFF;
                 self.pc = nnn;
             }
             //2NNN Execute subroutine starting at address NNN
             0x2000 => {
-                let nnn = opcode & 0x0FFF;
                 self.stack[self.sp as usize] = self.pc;
                 self.sp += 1;
                 self.pc = nnn;
             }
             //3XNN Skip the following instruction if the value of register VX equals NN
             0x3000 => {
-                let x = (opcode & 0x0F00) >> 8;
-                let nn = opcode & 0x00FF;
                 if self.v[x as usize] == nn as u8 {
                     self.pc += 2;
                 }
             }
             //4XNN Skip the following instruction if the value of register VX is not equal to NN
             0x4000 => {
-                let x = (opcode & 0x0F00) >> 8;
-                let nn = opcode & 0x00FF;
                 if self.v[x as usize] != nn as u8 {
                     self.pc += 2;
                 }
             }
             //5XY0 Skip the following instruction if the value of register VX is equal to the value of register VY
             0x5000 => {
-                let x = (opcode & 0x0F00) >> 8;
-                let y = (opcode & 0x00F0) >> 4;
                 if self.v[x as usize] == self.v[y as usize] {
                     self.pc += 2;
                 }
             }
             //6XNN Store number NN in register VX
             0x6000 => {
-                let x = (opcode & 0x0F00) >> 8;
-                let nn = opcode & 0x00FF;
                 self.v[x as usize] = nn as u8;
             }
             //7XNN Add the value NN to register VX
             0x7000 => {
-                let x = (opcode & 0x0F00) >> 8;
-                let nn = opcode & 0x00FF;
                 self.v[x as usize] = self.v[x as usize].wrapping_add(nn as u8);
             }
             0x8000 => match opcode & 0x000F {
                 //8XY0 Store the value of register VY in register VX
                 0x0 => {
-                    let x = (opcode & 0x0F00) >> 8;
-                    let y = (opcode & 0x00F0) >> 4;
                     self.v[x as usize] = self.v[y as usize];
                 }
                 //8XY1 Set VX to VX OR VY
                 0x1 => {
-                    let x = (opcode & 0x0F00) >> 8;
-                    let y = (opcode & 0x00F0) >> 4;
                     self.v[x as usize] |= self.v[y as usize];
                 }
                 //8XY2 Set VX to VX AND VY
                 0x2 => {
-                    let x = (opcode & 0x0F00) >> 8;
-                    let y = (opcode & 0x00F0) >> 4;
                     self.v[x as usize] &= self.v[y as usize];
                 }
                 //8XY3 Set VX to VX XOR VY
                 0x3 => {
-                    let x = (opcode & 0x0F00) >> 8;
-                    let y = (opcode & 0x00F0) >> 4;
                     self.v[x as usize] ^= self.v[y as usize];
                 }
                 //8XY4 Add the value of register VY to register VX
                 //Set VF to 01 if a carry occurs
                 //Set VF to 00 if a carry does not occur
                 0x04 => {
-                    let x = (opcode & 0x0F00) >> 8;
-                    let y = (opcode & 0x00F0) >> 4;
                     self.v[0xF] = 0;
                     let (sum, carry) = self.v[x as usize].overflowing_add(self.v[y as usize]);
                     if carry {
@@ -180,8 +164,6 @@ impl Cpu {
                 //Set VF to 00 if a borrow occurs
                 //Set VF to 01 if a borrow does not occur
                 0x05 => {
-                    let x = (opcode & 0x0F00) >> 8;
-                    let y = (opcode & 0x00F0) >> 4;
                     self.v[0xF] = 1;
                     let (sub, borrow) = self.v[x as usize].overflowing_sub(self.v[y as usize]);
                     if borrow {
@@ -192,9 +174,7 @@ impl Cpu {
                 //8XY6 Store the value of register VY shifted right one bit in register VX
                 //Set register VF to the least significant bit prior to the shift
                 0x06 => {
-                    let x = (opcode & 0x0F00) >> 8;
                     if !self.shift_quirk {
-                        let y = (opcode & 0x00F0) >> 4;
                         self.v[0xF] = self.v[y as usize] & 0x1;
                         self.v[y as usize] >>= 1;
                         self.v[x as usize] = self.v[y as usize];
@@ -207,8 +187,6 @@ impl Cpu {
                 //Set VF to 00 if a borrow occurs
                 //Set VF to 01 if a borrow does not occur
                 0x07 => {
-                    let x = (opcode & 0x0F00) >> 8;
-                    let y = (opcode & 0x00F0) >> 4;
                     self.v[0xF] = 1;
                     let (sub, borrow) = self.v[y as usize].overflowing_sub(self.v[x as usize]);
                     if borrow {
@@ -219,10 +197,8 @@ impl Cpu {
                 //8XYE Store the value of register VY shifted left one bit in register VX
                 //Set register VF to the most significant bit prior to the shift
                 0x0E => {
-                    let x = (opcode & 0x0F00) >> 8;
                     if !self.shift_quirk {
                         if !self.shift_quirk {
-                            let y = (opcode & 0x00F0) >> 4;
                             self.v[0xF] = (self.v[y as usize] >> 7) & 0x1;
                             self.v[y as usize] <<= 1;
                             self.v[x as usize] = self.v[y as usize];
@@ -237,35 +213,26 @@ impl Cpu {
             //9XY0 Skip the following instruction if the value of register VX
             //is not equal to the value of register VY
             0x9000 => {
-                let x = (opcode & 0x0F00) >> 8;
-                let y = (opcode & 0x00F0) >> 4;
                 if self.v[x as usize] != self.v[y as usize] {
                     self.pc += 2;
                 }
             }
             //ANNN Store memory address NNN in register I
             0xA000 => {
-                let nnn = opcode & 0x0FFF;
                 self.i = nnn;
             }
             //BNNN Jump to address NNN + V0
             0xB000 => {
-                let nnn = opcode & 0x0FFF;
                 self.pc = nnn + u16::from(self.v[0x0]);
             }
             //CXNN Set VX to a random number with a mask of NN
             0xC000 => {
-                let x = (opcode & 0x0F00) >> 8;
-                let nn = opcode & 0x00FF;
                 self.v[x as usize] = rand::thread_rng().gen::<u8>() & nn as u8;
             }
             //DXYN Draw a sprite at position VX, VY with N bytes of sprite data
             //starting at the address stored in I
             //Set VF to 01 if any set pixels are changed to unset, and 00 otherwise
             0xD000 => {
-                let x = (opcode & 0x0F00) >> 8;
-                let y = (opcode & 0x00F0) >> 4;
-                let n = opcode & 0x000F;
                 self.v[0xF] = 0;
                 for j in 0..n {
                     let p = self.memory[(self.i + j as u16) as usize];
@@ -288,7 +255,6 @@ impl Cpu {
                 //EX9E Skip the following instruction if the key corresponding to
                 //the hex value currently stored in register VX is pressed
                 0x9E => {
-                    let x = (opcode & 0x0F00) >> 8;
                     if self.keyboard[self.v[x as usize] as usize] != 0 {
                         self.pc += 2;
                     }
@@ -296,7 +262,6 @@ impl Cpu {
                 //EXA1 Skip the following instruction if the key corresponding
                 //to the hex value currently stored in register VX is not pressed
                 0xA1 => {
-                    let x = (opcode & 0x0F00) >> 8;
                     if self.keyboard[self.v[x as usize] as usize] == 0 {
                         self.pc += 2;
                     }
@@ -306,12 +271,10 @@ impl Cpu {
             0xF000 => match opcode & 0x00FF {
                 // FX07 Store the current value of the delay timer in register VX
                 0x07 => {
-                    let x = (opcode & 0x0F00) >> 8;
                     self.v[x as usize] = self.delay_timer;
                 }
                 //FX0A Wait for a keypress and store the result in register VX
                 0x0A => {
-                    let x = (opcode & 0x0F00) >> 8;
                     self.key_pressed = false;
                     for i in 0..self.keyboard.len() {
                         if self.keyboard[i] != 0 {
@@ -326,29 +289,24 @@ impl Cpu {
                 }
                 //FX15 Set the delay timer to the value of register VX
                 0x15 => {
-                    let x = (opcode & 0x0F00) >> 8;
                     self.delay_timer = self.v[x as usize];
                 }
                 //FX18 Set the sound timer to the value of register VX
                 0x18 => {
-                    let x = (opcode & 0x0F00) >> 8;
                     self.sound_timer = self.v[x as usize];
                 }
                 //FX1E Add the value stored in register VX to register I
                 0x1E => {
-                    let x = (opcode & 0x0F00) >> 8;
                     self.i += u16::from(self.v[x as usize]);
                 }
                 //FX29 Set I to the memory address of the sprite data corresponding to
                 //the hexadecimal digit stored in register VX
                 0x29 => {
-                    let x = (opcode & 0x0F00) >> 8;
-                    self.i = u16::from(self.v[x as usize]) * 0x05;
+                    self.i = u16::from(self.v[x as usize]) * 0x5;
                 }
                 //FX33 	Store the binary-coded decimal equivalent of the value stored
                 //in register VX at addresses I, I+1, and I+2
                 0x33 => {
-                    let x = (opcode & 0x0F00) >> 8;
                     self.memory[self.i as usize] = self.v[x as usize] / 100;
                     self.memory[(self.i + 1) as usize] = (self.v[x as usize] / 10) % 10;
                     self.memory[(self.i + 2) as usize] = (self.v[x as usize] % 100) % 10;
@@ -356,7 +314,6 @@ impl Cpu {
                 //FX55 Store the values of registers V0 to VX inclusive in memory starting at address I
                 //I is set to I + X + 1 after operation
                 0x55 => {
-                    let x = (opcode & 0x0F00) >> 8;
                     for i in 0..x as usize + 1 {
                         self.memory[self.i as usize + i] = self.v[i];
                     }
@@ -368,7 +325,6 @@ impl Cpu {
                 //stored in memory starting at address I
                 //I is set to I + X + 1 after operation
                 0x65 => {
-                    let x = (opcode & 0x0F00) >> 8;
                     for i in 0..x as usize + 1 {
                         self.v[i] = self.memory[self.i as usize + i];
                     }
